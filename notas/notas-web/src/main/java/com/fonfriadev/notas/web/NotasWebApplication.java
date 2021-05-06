1813cec2715ace07d0cc8b1800cf0b65f8ed0ddd
leina <leina@DANI-ACER>
2021-05-06 11:30:59 +0200

    *

---
+++ notas/notas-web/src/main/java/com/fonfriadev/notas/web/NotasWebApplication.java
@@ -0,0 +1,13 @@
+package com.fonfriadev.notas.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotasWebApplication.class, args);
+	}
+
+}
